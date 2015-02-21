a0c2cb2fb6a106629754dfe70bee4fa7912e8311
Adam Scott <adam.scott@cfpb.gov>
2015-02-21 11:47:52 -0500

    fix client build

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 var React = require('react/addons');
-var ReactIndex = React.createFactory(require('../components/ReactIndex').ReactIndex);
+var ReactIndex = React.createFactory(require('./components/ReactIndex').ReactIndex);
 
 var mountNode = document.getElementById("react-main-mount");
 
